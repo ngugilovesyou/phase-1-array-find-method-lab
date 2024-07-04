da75011471df99f8396b1b94aa68a6aaa22f7361
Samuel <amsamgitau@gmail.com>
2024-07-04 08:53:03 +0300

    modify index.js

--- index.js
+++ index.js
@@ -3,8 +3,9 @@ const record = [
   { year: "2014", result: "N/A"},
   { year: "2013", result: "L"},
 ]
+
 const superbowlWin =(record) => {
   const result = record.find(record => record.result === "W" );
-  return result ? result.year : undefined;
+  return result ? result.year : undefined;; 
 }
 console.log(superbowlWin(record))
